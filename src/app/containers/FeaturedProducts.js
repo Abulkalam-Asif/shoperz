1bb166aa2e71f9a2dfc41fe2e48d0ce68b96b924
Macbook pro <macbookpro@Macbooks-MacBook-Pro.local>
2023-09-22 11:05:13 -0700

    fix the slider

--- src/app/containers/FeaturedProducts.js
+++ src/app/containers/FeaturedProducts.js
@@ -6,7 +6,22 @@ import item2Img from "../assets/item2.png";
 import item3Img from "../assets/item3.png";
 import item4Img from "../assets/item4.png";
 import item5Img from "../assets/item5.png";
+import leftarrow from "../assets/leftarrow.png";
+import nextarrow from "../assets/rightarrow.png";
+
+
 import Carousel from "react-bootstrap/Carousel";
+
+import Slider from 'react-slick';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+
+import { useRef } from "react";
+
+import Head from 'next/head';
+
 
 const FeaturedProducts = () => {
   const featuredProducts = [
@@ -42,27 +57,111 @@ const FeaturedProducts = () => {
       price: 1100,
     },
   ];
+
+  const sliderRef = useRef(null);
+
+
+  const previousSlide = () => {
+    sliderRef.current.slickPrev();
+  };
+
+  const nextSlide = () => {
+    sliderRef.current.slickNext();
+  };
+
+  const CustomPrevArrow = (props) => (
+    <button
+      className="custom-arrows prev-arrow"
+      onClick={props.onClick}
+      style={{ display: "none" }}
+    />
+  );
+
+  const CustomNextArrow = (props) => (
+    <button
+      className="custom-arrows next-arrow"
+      onClick={props.onClick}
+      style={{ display: "none" }}
+    />
+  );
+
+
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4, // Set this to the number of products you want to show in a row
+    slidesToScroll: 1, // Set this to 1 to scroll one product at a time
+    prevArrow: <CustomPrevArrow />,
+    nextArrow: <CustomNextArrow />,
+    responsive: [
+      {
+        breakpoint: 1024, // tablet breakpoint
+        settings: {
+          slidesToShow: 4, // Adjust for tablet view
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600, // mobile breakpoint
+        settings: {
+          slidesToShow: 2, // Adjust for mobile view
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+
+
   return (
     <>
+
+      <Head>
+        <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+      </Head>
+
+
       <section className="py-16 px-8">
         <div className="max-w-8xl mx-auto space-y-9">
-          <H3 text={"Featured Products"} />
+
+
+          <div className="flex justify-between">
+
+            <H3 text={"Featured Products"} />
+
+    <div>
+
+            <button
+
+              onClick={previousSlide}
+            >
+              <img src={leftarrow} alt="leftarrow" />
+            </button>
+            <button onClick={nextSlide}>
+              <img src={nextarrow} alt="nextarrow" />
+            </button>
+            </div>
+
+
+          </div>
+
           <div>
-            <Carousel>
-              <Carousel.Item>
-                {featuredProducts.map(
-                  ({ companyName, productName, image, price }, index) => (
-                    <HomeProduct
-                      key={index}
-                      companyName={companyName}
-                      productName={productName}
-                      image={image}
-                      price={price}
-                    />
-                  ),
-                )}
-              </Carousel.Item>
-            </Carousel>
+            <Slider {...settings} ref={sliderRef}>
+              {featuredProducts.map(
+                ({ companyName, productName, image, price }, index) => (
+                  <HomeProduct
+                    key={index}
+                    companyName={companyName}
+                    productName={productName}
+                    image={image}
+                    price={price}
+                  />
+                ),
+              )}
+            </Slider>
           </div>
         </div>
       </section>
